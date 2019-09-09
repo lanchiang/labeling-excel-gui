71a8f7bc4e60196f74927737f36abad337eb76cd
Lan Jiang <jianglan1350@gmail.com>
2019-09-09 21:12:39 +0200

    add postgres to pom.xml; add abstract queries class

---
+++ src/main/java/de/hpi/isg/dao/AbstractQueries.java
@@ -0,0 +1,10 @@
+package de.hpi.isg.dao;
+
+/**
+ * This class defines a bunch of queries to the database.
+ *
+ * @author Lan Jiang
+ * @since 9/9/19
+ */
+abstract public class AbstractQueries {
+}
